c23dded025e9da4644749d8ef016abf194fffed9
Martin Poulsen <mapo0004@stud.kea.dk>
2024-03-07 09:28:50 +0100

    First Commit

---
+++ src/main/java/stud/kea/dk/scrapbasketsultimateproject/ScrapbasketsUltimateProjectApplication.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.scrapbasketsultimateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapbasketsUltimateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapbasketsUltimateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stud/kea/dk/scrapbasketsultimateproject/ScrapbasketsUltimateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.scrapbasketsultimateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapbasketsUltimateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
